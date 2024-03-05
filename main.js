e36aef2c2306e84dc20afd2e1932e24680ea3a50
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-03-05 09:03:57 -0300

    vd

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ alert('fuck fuck');
 let nomeUsuario = prompt('Qual seu nome?');
 let anterior = document.querySelector('#texto');
 
+while(nomeUsuario == "" ){
+    nomeUsuario = prompt('Qual seu nome?');
+}
+
 anterior.textContent = nomeUsuario;
