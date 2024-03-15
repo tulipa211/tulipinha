342ce846189ee7d63a8ef935882f97e682ff92e6
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-03-15 11:36:00 -0300

    g

--- joguines.js
+++ joguines.js
@@ -1,4 +1,11 @@
 let numeroSecreto = 9;
 
-let chute = prompt('Escolha u número de 0 a 10');
+let chute = prompt('Escolha um número de 0 a 10');
 
+if(numeroSecreto == chute){
+    alert('Acertou, boaa');
+    console.log('Parabéns voê acerotou, you did it!!')    
+} else{
+    alert('Errado');
+    console.log('separe aude, vc errou')
+}
