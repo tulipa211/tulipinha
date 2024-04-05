6585c10f4f577e58096d71c06ff8c28938507001
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-04-05 12:18:46 -0300

    .

--- cont.js
+++ cont.js
@@ -4,6 +4,7 @@ const botoes = document.querySelectorAll('.Objetivos');
 for (let x = 0; x<botoes.length; x++){
 console.log(x);
 botoes[x].onclick = function() {
+    for(let y = 0; y< botoes.length; y++)
 botoes[x].classList.add('ativo');
 }
 //botoes[x].classList.remove('ativo')
