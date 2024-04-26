25c49713987055dc2b8c7312916bfa8b5860f120
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-04-26 09:17:04 -0300

    .

--- cont.js
+++ cont.js
@@ -33,7 +33,9 @@ let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
 
-
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
 
 return dias + " dias " + horas + " horas " + minutos + " minutos " + minutos + " minutos " + segundos + " segundos ";
 }
