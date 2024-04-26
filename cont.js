3dfa785e3958393f25240933f17441049ea19c99
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-04-26 08:43:08 -0300

    .

--- cont.js
+++ cont.js
@@ -10,3 +10,13 @@ botoes[y].classList.remove('ativo');
 botoes[x].classList.add('ativo')
 //botoes[x].classList.remove('ativo')
 }}
+
+const contadores = document.querySelectorAll('contador');
+let tempoAtual = new Date ();
+let tempoObjetivo1 = new Date ('2024-04-28T23:59:99');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+let segundos = tempoFinal/1000; 
+
+
+contadores[0].textContent = segundos;
